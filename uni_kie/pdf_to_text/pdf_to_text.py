4d294241a9e649e3b5f3e699aa77a7b033114edd
ivo-1 <ivoschaper@gmail.com>
2022-10-20 15:24:14 +0200

    add pdftotext model abstraction

---
+++ uni_kie/pdf_to_text/pdf_to_text.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+# import pdftotext
+
+
+class AbstractPDFToTextModel:
+    def __init__(self):
+        pass
+
+    def get_text(self, file_path: Path) -> str:
+        raise NotImplementedError
+
+
+class PDFToTextWrapperModel(AbstractPDFToTextModel):
+    def __init__(self):
+        super().__init__()
+
+    def get_text(self, file_path: Path) -> str:
+        # Load your PDF
+        with open("lorem_ipsum.pdf", "rb") as f:
+            pdf = pdftotext.PDF(f)
+
+        # If it's password-protected
+        with open("secure.pdf", "rb") as f:
+            pdf = pdftotext.PDF(f, "secret")
+
+        # How many pages?
+        print(len(pdf))
+
+        # Iterate over all the pages
+        for page in pdf:
+            print(page)
+
+        # Read some individual pages
+        print(pdf[0])
+        print(pdf[1])
+
+        # Read all the text into one string
+        print("\n\n".join(pdf))
+
+
+if __name__ == "__main__":
+    pdf_to_text = PDFToTextWrapperModel()
+    pdf_to_text.get_text("lorem_ipsum.pdf")
--- uni_kie/pipeline.py
+++ uni_kie/pipeline.py
@@ -5,18 +5,13 @@ from uni_kie.constants import MODELS, OCR_MODELS, PARSERS, PROMPT_VARIANTS
 from uni_kie.models.baseline import BaselineModel
 from uni_kie.models.model import AbstractModel, LargeLanguageModel
 from uni_kie.ocr.ocr import AbstractOCRModel
+from uni_kie.pdf_to_text.pdf_to_text import AbstractPDFToTextModel
 
 
 class AbstractPipeline:
-    def __init__(self, model: AbstractModel, ocr_model: AbstractOCRModel):
+    def __init__(self, model: AbstractModel, pdf_to_text_model: AbstractPDFToTextModel):
         self.model = model
-        self.ocr_model = ocr_model
-
-    def _run_ocr(self, file_path: Union[str, Path]) -> str:
-        if isinstance(file_path, str):
-            file_path = Path(file_path)
-
-        return self.ocr_model.ocr(file_path)
+        self.pdf_to_text_model = pdf_to_text_model
 
     def predict(self, file_path: Union[str, Path]) -> dict:
         raise NotImplementedError
@@ -39,7 +34,7 @@ class LLMPipeline(AbstractPipeline):
     def __init__(
         self,
         model: LargeLanguageModel,
-        ocr_model: AbstractOCRModel,
+        pdf_to_text_model: AbstractPDFToTextModel,
         prompt_variant: PROMPT_VARIANTS,
         parser: PARSERS,
     ):
@@ -47,19 +42,19 @@ class LLMPipeline(AbstractPipeline):
         Initializes the pipeline.
 
         :param model: The model to use for inference.
-        :param ocr_model: The OCR model to use for inference.
+        :param pdf_to_text_model: The OCR model to use for inference.
         :param prompt_variant: The prompt variant to use for inference.
         :param parser: The parser to use for inference.
 
         :type model: MODELS
-        :type ocr_model: AbstractOCRModel
+        :type pdf_to_text_model: AbstractPDFToTextModel
         """
-        super().__init__(model, ocr_model)
+        super().__init__(model, pdf_to_text_model)
         self.prompt_variant = prompt_variant
         self.parser = parser
 
     def __repr__(self):
-        return f"LLMPipeline(ocr_model={self.ocr_model}, prompt_variant={self.prompt_variant}, model={self.model}, parser={self.parser})"
+        return f"LLMPipeline(pdf_to_text_model={self.pdf_to_text_model}, prompt_variant={self.prompt_variant}, model={self.model}, parser={self.parser})"
 
     def _generate_prompt(self, ocr_text: str) -> str:
         return ocr_text
@@ -71,8 +66,8 @@ class LLMPipeline(AbstractPipeline):
         return model_output
 
     def predict(self, file_path: Union[str, Path]):
-        ocr_text = self._run_ocr(file_path)
-        prompt = self._generate_prompt(ocr_text)
+        text = self.pdf_to_text_model.get_text(file_path)
+        prompt = self._generate_prompt(text)
         model_output = self._prompt_model(prompt)
         parsed_output = self._parse_model_output(model_output)
         return parsed_output
@@ -83,17 +78,17 @@ class LLMPipeline(AbstractPipeline):
 
 class BaselinePipeline(AbstractPipeline):
     def __init__(self, ocr_model):
-        super().__init__(model=BaselineModel(), ocr_model=ocr_model)
+        super().__init__(model=BaselineModel(), pdf_to_text_model=ocr_model)
 
     def __repr__(self):
-        return f"BaselinePipeline(ocr_model={self.ocr_model}, model={self.model})"
+        return f"BaselinePipeline(pdf_to_text_model={self.pdf_to_text_model}, model={self.model})"
 
     def predict(self, ocr_text: str):
         return self.model.predict(ocr_text)
 
     def predict_file(self, file_path: Union[str, Path]):
-        ocr_text = self._run_ocr(file_path)
-        model_output = self.predict(ocr_text)
+        text = self.pdf_to_text_model.get_text(file_path)
+        model_output = self.predict(text)
         return model_output
 
     def predict_directory(self, directory_path: Union[str, Path]):
