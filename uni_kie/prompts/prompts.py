5a727a9569e21172faf4574ef2b84c59647c6c9d
ivo-1 <ivoschaper@gmail.com>
2022-11-03 15:33:34 +0100

    fix generate_prompt type

--- uni_kie/prompts/prompts.py
+++ uni_kie/prompts/prompts.py
@@ -5,5 +5,5 @@ from uni_kie.constants import PROMPT_VARIANTS
 
 class Prompts:
     @staticmethod
-    def generate_prompt(document_ocr: str, variant: Union[PROMPT_VARIANTS]) -> str:
+    def generate_prompt(document_ocr: str, variant: PROMPT_VARIANTS) -> str:
         return document_ocr
