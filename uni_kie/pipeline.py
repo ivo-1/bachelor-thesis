67258e0f572fd1e698ef10dcc59a661a8949bf69
ivo-1 <ivoschaper@gmail.com>
2022-11-06 19:34:26 +0100

    parsers are now part of pipeline objects

--- uni_kie/pipeline.py
+++ uni_kie/pipeline.py
@@ -1,22 +1,30 @@
 from pathlib import Path
 from typing import List, Union
 
-from uni_kie.constants import MODELS, PARSERS, PROMPT_VARIANTS, TOKENIZERS
+from uni_kie.constants import MODELS, NER_TAGGERS, PARSERS, PROMPT_VARIANTS, TOKENIZERS
 from uni_kie.models.baseline import BaselineModel
 from uni_kie.models.model import AbstractModel, LargeLanguageModel
-from uni_kie.parser import Parser
+from uni_kie.parsers.parser import Parser
 from uni_kie.pdf_to_text.pdf_to_text import AbstractPDFToTextModel
 
 
 class AbstractPipeline:
-    def __init__(self, model: AbstractModel, pdf_to_text_model: AbstractPDFToTextModel):
+    def __init__(
+        self,
+        model: AbstractModel,
+        pdf_to_text_model: AbstractPDFToTextModel,
+        parser: Parser,
+    ):
         self.model = model
         self.pdf_to_text_model = pdf_to_text_model
+        self.parser = parser
 
-    def predict(self, file_path: Union[str, Path], gold_keys: List) -> dict:
+    def predict(self, file_path: Union[str, Path], keys: List[str]) -> dict:
         raise NotImplementedError
 
-    def predict_directory(self, directory_path: Union[str, Path]) -> List[dict]:
+    def predict_directory(
+        self, directory_path: Union[str, Path], keys: List[str]
+    ) -> List[dict]:
         raise NotImplementedError
 
 
@@ -24,7 +32,7 @@ class LLMPipeline(AbstractPipeline):
     """
     Consists of OCR_MODEL, PROMPT_VARIANT, MODEL, PARSER.
     # 1. takes in some file
-    # 2. runs OCR_MODELS on it
+    # 2. gets the text (either from OCR or from the dataset)
     # 3. creates a prompt with the OCR_MODELS'd text
     # 4. prompts the model with the prompt
     # 5. parses the model's output
@@ -48,11 +56,11 @@ class LLMPipeline(AbstractPipeline):
         :type model: MODELS
         :type pdf_to_text_model: AbstractPDFToTextModel
         :type prompt_variant: PROMPT_VARIANTS
-        :type parser: Parser
         """
-        super().__init__(model, pdf_to_text_model)
+        super().__init__(
+            model=model, pdf_to_text_model=pdf_to_text_model, parser=parser
+        )
         self.prompt_variant = prompt_variant
-        self.parser = parser
 
     def __repr__(self):
         return f"LLMPipeline(pdf_to_text_model={self.pdf_to_text_model}, prompt_variant={self.prompt_variant}, model={self.model}, parser={self.parser})"
@@ -78,33 +86,44 @@ class LLMPipeline(AbstractPipeline):
 
         return prompt
 
-    def _parse_model_output(self, model_output: str, gold_keys) -> Union[dict, str]:
-        return self.parser.parse_single_model_output(model_output, gold_keys)
-
     def predict(
         self, file_path: Union[str, Path], prompt_keys: List[str]
     ) -> Union[dict, str]:
         text = self.pdf_to_text_model.get_text(file_path)
         prompt = self._generate_prompt(text, prompt_keys)
         model_output = self.model.predict(prompt)
-        parsed_output = self._parse_model_output(model_output, prompt_keys)
+        parsed_output = self.parser.parse_single_model_output(model_output, prompt_keys)
         return parsed_output
 
-    def predict_directory(self, directory_path: Union[str, Path]):
+    def predict_directory(
+        self, directory_path: Union[str, Path], prompt_keys: List[str]
+    ) -> Union[List[dict], List[str]]:
         pass
 
 
 class BaselinePipeline(AbstractPipeline):
-    def __init__(self, pdf_to_text_model: AbstractPDFToTextModel):
-        super().__init__(model=BaselineModel(), pdf_to_text_model=pdf_to_text_model)
+    def __init__(
+        self,
+        pdf_to_text_model: AbstractPDFToTextModel,
+        parser: Parser,
+        ner_tagger: NER_TAGGERS,
+    ):
+        super().__init__(
+            model=BaselineModel(ner_tagger=ner_tagger),
+            pdf_to_text_model=pdf_to_text_model,
+            parser=parser,
+        )
 
     def __repr__(self):
         return f"BaselinePipeline(pdf_to_text_model={self.pdf_to_text_model}, model={self.model})"
 
-    def predict(self, file_path: Union[str, Path]):
+    def predict(self, file_path: Union[str, Path], keys: List[str]) -> Union[dict, str]:
         text = self.pdf_to_text_model.get_text(file_path)
-        prediction = self.model.predict(text)
-        return prediction
+        model_output = self.model.predict(text, keys)
+        parsed_output = self.parser.parse_single_model_output(model_output, keys)
+        return parsed_output
 
-    def predict_directory(self, directory_path: Union[str, Path]):
+    def predict_directory(
+        self, directory_path: Union[str, Path], keys: List[str]
+    ) -> Union[List[dict], List[str]]:
         pass
