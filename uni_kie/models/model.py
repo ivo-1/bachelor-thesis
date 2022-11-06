8c43157320a2cacacabaf1cad8f593ad71162328
ivo-1 <ivoschaper@gmail.com>
2022-11-06 19:45:52 +0100

    remove max_input_tokens from abstract model and allow for different args in predict()

--- uni_kie/models/model.py
+++ uni_kie/models/model.py
@@ -1,14 +1,15 @@
 class AbstractModel:
     def __init__(self):
-        self.max_input_tokens: int
+        pass
 
-    def predict(self, input: str) -> str:
+    def predict(self, *args, **kwargs) -> str:
         raise NotImplementedError
 
 
 class LargeLanguageModel(AbstractModel):
     def __init__(self):
         super().__init__()
+        self.max_input_tokens: int
 
     def predict(self, input: str) -> str:
-        return input
+        raise NotImplementedError
