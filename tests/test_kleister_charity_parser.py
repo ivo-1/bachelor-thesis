31401056669f1514cf542e76c7e56416392ba59c
ivo-1 <ivoschaper@gmail.com>
2022-11-16 16:57:44 +0100

    add test for money parser

--- tests/test_kleister_charity_parser.py
+++ tests/test_kleister_charity_parser.py
@@ -15,8 +15,7 @@ dev_example_expected_output_with_last_key = (
     "address__postcode=SS0_8HX "
     "address__street_line=47_SECOND_AVENUE charity_name=Havens_Christian_Hospice "
     "income_annually_in_british_pounds=10348000.00 "
-    "report_date=2016-03-31 "
-    "spending_annually_in_british_pounds=null_FOO_BAR"
+    "report_date=2016-03-31"
 )
 
 
@@ -66,3 +65,15 @@ def test_parse_single_model_output_empty():
         model_output, KLEISTER_CHARITY_CONSTANTS.prompt_keys
     )
     assert parsed_output == ""
+
+
+def test_parse_single_model_output_money_parser():
+    model_output = "  £1,034,800.00\nAnnual Income: \n\n  £12 345\nReport Date: 2016-03-31\nAnnual Spending:  £1,034,800.23"
+    parser = PARSERS.KLEISTER_CHARITY_PARSER
+    parsed_output = parser.parse_model_output(
+        model_output, KLEISTER_CHARITY_CONSTANTS.prompt_keys
+    )
+    assert (
+        parsed_output
+        == "address__post_town=£1,034,800.00 income_annually_in_british_pounds=12345.00 report_date=2016-03-31 spending_annually_in_british_pounds=1034800.23"
+    )
