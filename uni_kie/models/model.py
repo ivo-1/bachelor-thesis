b1d1312469f12458e3118717da4079db03fc921a
ivo-1 <ivoschaper@gmail.com>
2022-11-18 20:19:38 +0100

    fix __repr__

--- uni_kie/models/model.py
+++ uni_kie/models/model.py
@@ -5,6 +5,9 @@ class AbstractModel:
     def predict(self, *args, **kwargs) -> str:
         raise NotImplementedError
 
+    def __repr__(self) -> str:
+        return self.__class__.__name__
+
 
 class LargeLanguageModel(AbstractModel):
     def __init__(self):
@@ -12,7 +15,7 @@ class LargeLanguageModel(AbstractModel):
         self.max_input_tokens: int
 
     def __repr__(self):
-        return self.__class__.__name__
+        return super().__repr__
 
     def predict(self, input: str) -> str:
         raise NotImplementedError
