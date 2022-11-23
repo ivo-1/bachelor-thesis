bc38947d0d650a8fbc7b06ea58b632ce135bdcb0
ivo-1 <ivoschaper@gmail.com>
2022-11-23 17:32:20 +0100

    add another test

--- tests/test_kleister_charity_parser.py
+++ tests/test_kleister_charity_parser.py
@@ -96,10 +96,10 @@ def test__dict_to_kleister_charity():
     assert translated_parsed_output == dev_example_expected_output
 
 
-# def test_parse_single_model_output():
-#     model_output = " \nAddress (post code):\nAddress (street):\nCharity name:\nCharity number:\nAnnual income:\nPeriod end date:\nAnnual spending:."
-#     parser = PARSERS.KLEISTER_CHARITY_PARSER
-#     parsed_output = parser.parse_model_output(
-#         model_output, KLEISTER_CHARITY_CONSTANTS.prompt_keys
-#     )
-#     assert parsed_output == "spending_annually_in_british_pounds=."
+def test_parse_single_model_output_another_one():
+    model_output = " \nAddress (post code):\nAddress (street):\nCharity name:\nCharity number:\nAnnual income:\nPeriod end date:\nAnnual spending:\n."
+    parser = PARSERS.KLEISTER_CHARITY_PARSER
+    parsed_output = parser.parse_model_output(
+        model_output, KLEISTER_CHARITY_CONSTANTS.prompt_keys
+    )
+    assert parsed_output == "spending_annually_in_british_pounds=."
