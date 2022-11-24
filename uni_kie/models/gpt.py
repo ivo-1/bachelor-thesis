17ad718c8b8ca7cb2eeec43fff59f533155b3f70
ivo-1 <ivoschaper@gmail.com>
2022-11-24 01:56:38 +0100

    add reason for why top_p not default

--- uni_kie/models/gpt.py
+++ uni_kie/models/gpt.py
@@ -19,7 +19,7 @@ class GPT3_Davinci(LargeLanguageModel):
         self.max_input_tokens = 3840
         self.max_generated_tokens = 256
         self.temperature = 1  # default: 1
-        self.top_p = 0.9  # default: 1
+        self.top_p = 0.9  # default: 1 but using 0.9 to compare to gpt-neox
         self.presence_penalty = 0  # default: 0
         self.frequency_penalty = 0  # default: 0
         self.stop = [STOP_KEY]
