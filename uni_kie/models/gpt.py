5df1d3ee98debeb086cfba101e56e929f88edfcd
ivo-1 <ivoschaper@gmail.com>
2022-12-10 13:53:03 +0100

    gpt3 run with temperature=0

--- uni_kie/models/gpt.py
+++ uni_kie/models/gpt.py
@@ -18,7 +18,7 @@ class GPT3_Davinci(LargeLanguageModel):
         # babbage + curie: 2048 - 256 = 1792
         self.max_input_tokens = 3840
         self.max_generated_tokens = 256
-        self.temperature = 1  # default: 1
+        self.temperature = 0  # default: 1
         self.top_p = 0.9  # default: 1 but using 0.9 to compare to gpt-neox
         self.presence_penalty = 0  # default: 0
         self.frequency_penalty = 0  # default: 0
