add03dff66411e0263035c5a4efde1e5d2174791
ivo-1 <ivoschaper@gmail.com>
2023-01-13 19:44:54 +0100

    add another test

--- tests/test_kleister_charity_parser.py
+++ tests/test_kleister_charity_parser.py
@@ -80,6 +80,18 @@ def test_parse_single_model_output_money_parser():
     )
 
 
+def test_parse_single_model_output_money_parser_ii():
+    model_output = "  null\nAnnual Income: \n\n  null\n Period End Date: 31 December 2015\nAnnual Spending:  £19.4m"
+    parser = PARSERS.KLEISTER_CHARITY_PARSER
+    parsed_output = parser.parse_model_output(
+        model_output, KLEISTER_CHARITY_CONSTANTS.prompt_keys
+    )
+    assert (
+        parsed_output
+        == "report_date=2015-12-31 spending_annually_in_british_pounds=19.4"
+    )
+
+
 def test__dict_to_kleister_charity():
     model_output = (
         " null\nAddress (post code): SS0 8HX\n\nAddress (street):    47:SECOND AVENUE\nCharity Name: "
