ec74e7a4518709f0ae7076322ba353b5eba5c5a3
ivo-1 <ivoschaper@gmail.com>
2023-01-29 00:15:30 +0100

    Update zipper.py

--- uni_kie/zipper.py
+++ uni_kie/zipper.py
@@ -1,5 +1,5 @@
 import shutil
 
-folder_path = ""
+folder_path = "/Users/ivo/PycharmProjects/Levity/unimodal-kie/uni_kie/datasets/sroie/test/predictions/flan-t5/2023-01-25T22-00-26_LLMPipeline(prompt_variant=NeutralPrompt, model=Flan_T5(max_input_tokens=1792, temperature=0, top_p=0.9, top_k=40), parser=DictParser, shots=TWOSHOT)"
 
 shutil.make_archive(folder_path, "zip", folder_path)
